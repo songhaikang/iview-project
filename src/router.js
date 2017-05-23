346ba44811b62339b90a1f08d208e993dd282c75
songhaikang <songhaikang@163.com>
2017-05-23 20:43:09 +0800

    学习单文件使用

--- src/router.js
+++ src/router.js
@@ -2,9 +2,27 @@ const routers = [
     {
         path: '/',
         meta: {
-            title: ''
+            title: '主页'
         },
         component: (resolve) => require(['./views/index.vue'], resolve)
+    },{
+        path: '/home/:username',
+        meta: {
+            title: '首页'
+        },
+        component: (resolve) => require(['./views/home.vue'], resolve)
+    },{
+        path: '/user_list',
+        meta: {
+            title: '用户信息列表'
+        },
+        component: (resolve) => require(['./views/user_list.vue'], resolve)
+    },{
+        path: '/user_edit',
+        meta: {
+            title: '用户信息编辑'
+        },
+        component: (resolve) => require(['./views/user_edit.vue'], resolve)
     }
 ];
 export default routers;
