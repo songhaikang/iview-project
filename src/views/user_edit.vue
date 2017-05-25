1b51761d7a18cac775711e515741763016c2020d
songhaikang <songhaikang@163.com>
2017-05-25 15:39:55 +0800

    数据可以提交成功

--- src/views/user_edit.vue
+++ src/views/user_edit.vue
@@ -3,49 +3,35 @@
         <Form-item label="姓名" prop="name">
             <Input v-model="formValidate.name" placeholder="请输入姓名"></Input>
         </Form-item>
-        <Form-item label="邮箱" prop="mail">
-            <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
+        <Form-item label="邮箱" prop="email">
+            <Input v-model="formValidate.email" placeholder="请输入邮箱"></Input>
         </Form-item>
-        <Form-item label="城市" prop="city">
-            <Select v-model="formValidate.city" placeholder="请选择所在地">
-                <Option value="beijing">北京市</Option>
-                <Option value="shanghai">上海市</Option>
-                <Option value="shenzhen">深圳市</Option>
+        <Form-item label="用户状态" prop="status">
+            <Select v-model="formValidate.status" placeholder="请选择用户状态">
+                <Option value="NORMAL">正常</Option>
+                <Option value="UN_ACTIVATE">未激活</Option>
+                <Option value="FORBIDDEN">禁用</Option>
             </Select>
         </Form-item>
-        <Form-item label="选择日期">
-            <Row>
-                <Col span="11">
-                <Form-item prop="date">
-                    <Date-picker type="date" placeholder="选择日期" v-model="formValidate.date"></Date-picker>
-                </Form-item>
-                </Col>
-                <Col span="2" style="text-align: center">
-                -</Col>
-                <Col span="11">
-                <Form-item prop="time">
-                    <Time-picker type="time" placeholder="选择时间" v-model="formValidate.time"></Time-picker>
-                </Form-item>
-                </Col>
-            </Row>
-        </Form-item>
+
         <Form-item label="性别" prop="gender">
             <Radio-group v-model="formValidate.gender">
-                <Radio label="male">男</Radio>
-                <Radio label="female">女</Radio>
+                <Radio label="MALE">男</Radio>
+                <Radio label="FEMALE">女</Radio>
             </Radio-group>
         </Form-item>
-        <Form-item label="爱好" prop="interest">
-            <Checkbox-group v-model="formValidate.interest">
-                <Checkbox label="吃饭"></Checkbox>
+        <Form-item label="爱好" prop="interestArray">
+            <Checkbox-group v-model="formValidate.interestArray">
+                <Checkbox label="吃饭">爱吃饭</Checkbox>
                 <Checkbox label="睡觉"></Checkbox>
                 <Checkbox label="跑步"></Checkbox>
                 <Checkbox label="看电影"></Checkbox>
             </Checkbox-group>
         </Form-item>
-        <Form-item label="介绍" prop="desc">
-            <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
+        <Form-item label="介绍" prop="description">
+            <Input v-model="formValidate.description" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
         </Form-item>
+
         <Form-item>
             <Button type="primary" @click="initFormData()">初始化数据</Button>
             <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
@@ -59,40 +45,32 @@
         data () {
             return {
                 formValidate: {
-                    name: 'sky',
-                    mail: '',
-                    city: '',
+                    name: '宋海康',
+                    email: '',
+                    status: '',
                     gender: '',
-                    interest: [],
-                    date: '',
-                    time: '',
-                    desc: ''
+                    interestArray: [],
+                    description: ''
                 },
                 ruleValidate: {
                     name: [
                         {required: true, message: '姓名不能为空', trigger: 'blur'}
                     ],
-                    mail: [
+                    email: [
                         {required: true, message: '邮箱不能为空', trigger: 'blur'},
                         {type: 'email', message: '邮箱格式不正确', trigger: 'blur'}
                     ],
-                    city: [
-                        {required: true, message: '请选择城市', trigger: 'change'}
+                    status: [
+                        {required: true, message: '请选择用户状态', trigger: 'change'}
                     ],
                     gender: [
                         {required: true, message: '请选择性别', trigger: 'change'}
                     ],
-                    interest: [
+                    interestArray: [
                         {required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change'},
                         {type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change'}
                     ],
-                    date: [
-                        {required: true, type: 'date', message: '请选择日期', trigger: 'change'}
-                    ],
-                    time: [
-                        {required: true, type: 'date', message: '请选择时间', trigger: 'change'}
-                    ],
-                    desc: [
+                    description: [
                         {required: true, message: '请输入个人介绍', trigger: 'blur'},
                         {type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur'}
                     ]
@@ -103,7 +81,7 @@
             initFormData(){
                 this.$http.post(
                     "/cep-svc/user/queryById.do",
-                    {"id":"1"},
+                    {"id": "1"},
                     {emulateJSON: true}
                 ).then(
                     function (res) {
@@ -119,10 +97,28 @@
             },
             handleSubmit (name) {
                 alert(JSON.stringify(this.formValidate));
-
+                var self = this;
                 this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('提交成功!');
+                    if (valid) {//如果验证通过
+                        this.$http.post(
+                            "/cep-svc/user/update.do",
+                            self.formValidate,
+                            {emulateJSON: true}
+                        ).then(
+                            function (res) {// 处理成功的结果
+                                alert(JSON.stringify(res));
+                                var result = JSON.parse(res.bodyText);
+                                if (result.code == "1") {
+                                    this.$Message.success('提交成功!');
+                                } else {
+                                    this.$Message.success('提交出错!');
+                                }
+                            }, function (res) {// 处理失败的结果
+                                alert(JSON.stringify(res));
+                            }
+                        );
+
+
                     } else {
 //                        debugger;
 //                        var formData = this.$refs[name].serialize;
